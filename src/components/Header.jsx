b9b2881b0b8a554a5f440601da5772286fd6f931
SFERAS <rthyssen73@gmail.com>
2023-01-27 09:46:37 -0300

    27.1.2023 start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,13 +13,13 @@ function Header() {
 						src='https://scontent.faep14-3.fna.fbcdn.net/v/t39.30808-6/280801944_558309148983574_2186507381685286523_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=hphpXi1DX_8AX9_RGI2&_nc_oc=AQlqYEn4bJ2PiTG2mipaRnFxc1gTRPRfNGIfjrZTdOOS15Xxhi7q5m3rbRa5GlXUW64&_nc_ht=scontent.faep14-3.fna&oh=00_AfBLZWypUzKQma7zVBZzcJhYDzyl8QxtvIV14gezgHR_Og&oe=63D4FD4C'
 						alt='logo' />
 				</a>
-				<h3>TELAS GROUP</h3>
+				<h2>TELAS GROUP</h2>
 
 			</div>
 
 
 			<div className='logos__container'>
-				<IconContext.Provider value={{ color: 'black', size: '20px' }}>
+				<IconContext.Provider value={{ color: 'black' }}>
 					<IconButton>
 						<FaInstagram />
 					</IconButton>
