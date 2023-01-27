d8e2f453c39796026b3e629dbb20b65dac672d56
SFERAS <rthyssen73@gmail.com>
2023-01-27 18:17:03 -0300

    27.1.2023 img problem done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
 			<div className="telas__logo">
 				<a href='#/' target="#" className='link__logo'>
 					<img className='header__logo'
-						src='https://scontent.faep14-3.fna.fbcdn.net/v/t39.30808-6/280801944_558309148983574_2186507381685286523_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=hphpXi1DX_8AX9_RGI2&_nc_oc=AQlqYEn4bJ2PiTG2mipaRnFxc1gTRPRfNGIfjrZTdOOS15Xxhi7q5m3rbRa5GlXUW64&_nc_ht=scontent.faep14-3.fna&oh=00_AfBLZWypUzKQma7zVBZzcJhYDzyl8QxtvIV14gezgHR_Og&oe=63D4FD4C'
+						src='images/telas_LOGO.jpg'
 						alt='logo' />
 				</a>
 				<h2>TELAS GROUP</h2>
